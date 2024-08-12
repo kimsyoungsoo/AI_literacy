c0d0a9702abc821c71e12821651edcdcdfb90b98
kimsyoungsoo <0soo1004@naver.com>
2024-08-12 16:54:44 +0900

    Update food_main.py

--- food_main.py
+++ food_main.py
@@ -6,13 +6,13 @@ import bunsik as bs
 
 def food_main():
         st.write("추천 맛집 종류예요: ")
-            menu_number = st.radio("음식추천", ["한식","패스트푸드","분식"],index=None)
+        menu_number = st.radio("음식추천", ["한식","패스트푸드","분식"],index=None)
         if menu_number == '한식':
             st.write("\"한식\"을 선택하셨네요!")
-       
+            kf.k_food():
         elif menu_number == '패스트푸드':
             st.write("\"패스트푸드\"를 선택하셨네요!")
-       
+            ff.fast_food():
         elif menu_number == '분식':
             st.write("\"분식\"을 선택하셨네요!")
-      
+            bs.bunsik():
