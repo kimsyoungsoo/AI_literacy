3c5d63b8c791a1b2e0b256bd447e35e131875985
kimsyoungsoo <0soo1004@naver.com>
2024-08-12 16:30:29 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
-import electric_car as ec  # lectric_car.py 파일에 있는 함수를 불러오는 것
+import electric_car as ec  # electric_car.py 파일에 있는 함수를 불러오는 것
 import pybasic  as pb
+import food_main as fm
 
 #로그인 화면
 st.sidebar.title(">> 로그인")    #왼쪽 title 글자크기
@@ -9,19 +10,18 @@ user_pw=st.sidebar.text_input('패스워드 입력',value='',type='password')
 
 if user_id == 'abc' and user_pw =='1234' :
    st.sidebar.title('>> 0soo의 포트폴리오')
-  #st.image('mojiseuyangjang5.jpg')
+  #st.image('data\mojiseuyangjang5.jpg')
 
-   menu=st.sidebar.radio('메뉴선택',['파이썬기초','탐색적 분석 : 전기자동차 분석','머신러닝'],index=None)
+   menu=st.sidebar.radio('메뉴선택',['파이썬기초','탐색적 분석 : 전기자동차 분석','머신러닝','파이썬기초 미니프로젝트'],index=None)
    st.sidebar.write(menu)
 
    if menu == '탐색적 분석 : 전기자동차 분석':
-      ec.elec_exe()
+     ec.elec_exe()
    elif menu == "머신러닝" :
      st.header("공사중")    
    elif menu == '파이썬기초':  
      pb.basic()                #함수 불러오기
-
-
-  
+   elif menu == '파이썬기초 미니프로젝트': 
+     fm.food_main()
   
                      
