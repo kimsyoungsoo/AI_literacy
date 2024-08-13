6a9963861539c0d9baf82cbd23f3cec454f6e1ce
kimsyoungsoo <0soo1004@naver.com>
2024-08-14 08:46:13 +0900

    Update food_main.py

--- food_main.py
+++ food_main.py
@@ -10,9 +10,18 @@ def food_main():
         if menu_number == '한식':
             st.write("\"한식\"을 선택하셨네요!")
             kf.k_food():
+        #   if not kf.k_food():            
+        #      st.write("다시 선택해 주세요")
+        #   else:
         elif menu_number == '패스트푸드':
             st.write("\"패스트푸드\"를 선택하셨네요!")
             ff.fast_food():
+        #  if not ff.fast_food():
+        #     st.write("다시 선택해 주세요")
+        #   else:
         elif menu_number == '분식':
             st.write("\"분식\"을 선택하셨네요!")
             bs.bunsik():
+         # if not bs.bunsik():
+         #   st.write("다시 선택해 주세요")
+             
