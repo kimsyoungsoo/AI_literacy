5b304afd3e1c2f0c541e9f5ed817af487ffe817b
kimsyoungsoo <0soo1004@naver.com>
2024-08-14 09:26:18 +0900

    Update main.py

--- main.py
+++ main.py
@@ -23,10 +23,8 @@ if user_id == 'abc' and user_pw =='1234' :
      pb.basic()                #함수 불러오기
    elif menu == '파이썬기초 미니프로젝트': 
      fm.food_main()
-   elif menu == '영수는 누구일까요?':
+   elif menu == '김영수를 소개합니다':
       menu=st.radio('김영수',['김','영','수'],index=None)
-      if menu == "영" :
-         st.write('영원히 사랑스러운')
-                
+     
   
                      
